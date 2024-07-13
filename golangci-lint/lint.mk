d630c5fa75ac34ae615f5fbe2deb19e0343b401a
akm <akm2000@gmail.com>
2024-07-13 21:12:43 +0900

    :sparkles: golangci-lint をインストールする golangci-lint/lint.mk を追加

---
+++ golangci-lint/lint.mk
@@ -0,0 +1,10 @@
+GOLANGCLI_LINT_CLI_VERSION?=latest
+GOLANGCLI_LINT_CLI=$(shell go env GOPATH)/bin/golangci-lint
+$(GOLANGCLI_LINT_CLI):
+	CLI_VERSION=$(GOLANGCLI_LINT_CLI_VERSION) \
+	CLI_MODULE=github.com/golangci/golangci-lint/cmd/golangci-lint \
+	$(MAKE) -C $(PATH_TO_SHAPEAPPMK)/golang/tool cli-install
+
+.PHONY: lint
+lint: $(GOLANGCLI_LINT_CLI)
+	$(GOLANGCLI_LINT_CLI) run ./...
