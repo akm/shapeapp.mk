4613c73081e1d9cc73f57f7dbd8964e08007dc8a
akm <akm2000@gmail.com>
2024-07-15 21:09:26 +0900

    :sparkles: ***_DEPS  にもプレフィックスを追加

--- sveltekit/npm.mk
+++ sveltekit/npm.mk
@@ -15,17 +15,17 @@ npm-ci:
 
 # TODO npm run build ではなく npm run check を使うように変更
 .PHONY: npm-run-build
-npm-run-build: $(PATH_TO_NODE_MODULES) $(BUILD_DEPS)
+npm-run-build: $(PATH_TO_NODE_MODULES) $(NPM_RUN_BUILD_DEPS)
 	npm run build
 
 .PHONY: npm-run-preview
-npm-run-preview: $(PATH_TO_NODE_MODULES) $(PREVIEW_DEPS)
+npm-run-preview: $(PATH_TO_NODE_MODULES) $(NPM_RUN_PREVIEW_DEPS)
 	$(DEV_ENVS) npm run preview
 
 .PHONY: npm-run-test
-npm-run-test: $(PATH_TO_NODE_MODULES) $(TEST_DEPS)
+npm-run-test: $(PATH_TO_NODE_MODULES) $(NPM_RUN_TEST_DEPS)
 	npm run test
 
 .PHONY: npm-run-lint
-npm-run-lint: $(PATH_TO_NODE_MODULES) $(LINT_DEPS)
+npm-run-lint: $(PATH_TO_NODE_MODULES) $(NPM_RUN_LINT_DEPS)
 	npm run lint
