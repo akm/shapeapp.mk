3c3e5c3cb71ab8a5d7c9fd83da360352a553e880
akm <akm2000@gmail.com>
2024-07-15 20:23:11 +0900

    :recycle: connect-web-install-dev-deps と connect-web-install-deps を抽出

--- connect-web/install.mk
+++ connect-web/install.mk
@@ -1,5 +1,10 @@
 .PHONY: connect-web-install
-connect-web-install:
-	npm install && \
-	npm install --save-dev @bufbuild/buf @connectrpc/protoc-gen-connect-es @bufbuild/protoc-gen-es && \
+connect-web-install: npm-install connect-web-install-dev-deps connect-web-install-deps
+
+.PHONY: connect-web-install-dev-deps
+connect-web-install-dev-deps:
+	npm install --save-dev @bufbuild/buf @connectrpc/protoc-gen-connect-es @bufbuild/protoc-gen-es
+
+.PHONY: connect-web-install-deps
+connect-web-install-deps:
 	npm install @connectrpc/connect @connectrpc/connect-web @bufbuild/protobuf
