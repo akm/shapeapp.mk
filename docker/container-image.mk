8d10fd10a52e59c4642d3f48519a6e83622cf8da
akm <akm2000@gmail.com>
2024-08-09 13:16:54 +0900

    🔨♻️ DOCKER_CONTAINER_IMAGE を DOCKER_IMAGE に置換

--- docker/container-image.mk
+++ docker/container-image.mk
@@ -1,21 +1,21 @@
 # Requires:
-# - DOCKER_CONTAINER_IMAGE_NAME
-# - DOCKER_CONTAINER_IMAGE_TAG
+# - DOCKER_IMAGE_NAME
+# - DOCKER_IMAGE_TAG
 #
 # Options:
-# - DOCKER_CONTAINER_IMAGE_BUILD_DEPS
-# - DOCKER_CONTAINER_IMAGE_BUILD_OPTS: Default: -f Dockerfile
+# - DOCKER_IMAGE_BUILD_DEPS
+# - DOCKER_IMAGE_BUILD_OPTS: Default: -f Dockerfile
 
-DOCKER_CONTAINER_IMAGE_BUILD_DEPS?=
-DOCKER_CONTAINER_IMAGE_BUILD_OPTS?=-f Dockerfile
+DOCKER_IMAGE_BUILD_DEPS?=
+DOCKER_IMAGE_BUILD_OPTS?=-f Dockerfile
 
 .PHONY: docker-container-image-name
 docker-container-image-name:
-	@echo $(DOCKER_CONTAINER_IMAGE_NAME)
+	@echo $(DOCKER_IMAGE_NAME)
 
 .PHONY: docker-container-image-build
-docker-container-image-build: $(DOCKER_CONTAINER_IMAGE_BUILD_DEPS)
+docker-container-image-build: $(DOCKER_IMAGE_BUILD_DEPS)
 	docker build \
-		-t $(DOCKER_CONTAINER_IMAGE_TAG) \
-		$(DOCKER_CONTAINER_IMAGE_BUILD_OPTS) \
+		-t $(DOCKER_IMAGE_TAG) \
+		$(DOCKER_IMAGE_BUILD_OPTS) \
 		.
