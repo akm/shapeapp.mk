3cf84ee55fe4d00b8de4f3042c0ef7dcc17792cb
akm <akm2000@gmail.com>
2024-08-09 13:18:46 +0900

    🔨♻️ docker-container-image を docker-image に置換

--- docker/container-image.mk
+++ docker/container-image.mk
@@ -9,12 +9,12 @@
 DOCKER_IMAGE_BUILD_DEPS?=
 DOCKER_IMAGE_BUILD_OPTS?=-f Dockerfile
 
-.PHONY: docker-container-image-name
-docker-container-image-name:
+.PHONY: docker-image-name
+docker-image-name:
 	@echo $(DOCKER_IMAGE_NAME)
 
-.PHONY: docker-container-image-build
-docker-container-image-build: $(DOCKER_IMAGE_BUILD_DEPS)
+.PHONY: docker-image-build
+docker-image-build: $(DOCKER_IMAGE_BUILD_DEPS)
 	docker build \
 		-t $(DOCKER_IMAGE_TAG) \
 		$(DOCKER_IMAGE_BUILD_OPTS) \
