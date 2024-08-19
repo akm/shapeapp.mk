ff7dbb1d39aff4fb6183686e9d8b72ddf2be6df8
akm <akm2000@gmail.com>
2024-08-19 20:57:41 +0900

    🔨 APISVR_TEST_VARS と APISVR_TEST_ENVS や test の定義を default/apisvr/test.mk に移動

--- default/apisvr/test.mk
+++ default/apisvr/test.mk
@@ -3,6 +3,15 @@
 # Options:
 #   - APISVR_TEST_ENVS
 
+# TEST_CONTAINERS-mysql-dsn
+$(call shell-dir-target-vars,TEST_CONTAINERS-,$(APISVR_TEST_PATH_TO_CONTAINERS),mysql-dsn)
+
+APISVR_TEST_VARS=$(APISVR_ENV_VARS)
+APISVR_TEST_ENVS=$(APISVR_ENVS_BASE) \
+	DB_DSN='$(TEST_CONTAINERS-mysql-dsn)' \
+	TEST_PATH_TO_FIXTURES=$(APISVR_TEST_PATH_TO_FIXTURES) \
+	$(foreach var,$(APISVR_TEST_VARS),$(var)=$($(var)))
+
 .PHONY: test-containers-up
 test-containers-up:
 	$(MAKE) -C $(APISVR_TEST_PATH_TO_CONTAINERS) up
@@ -14,3 +23,6 @@ test-containers-down:
 .PHONY: test-run
 test-run:
 	$(APISVR_TEST_ENVS) go test ./...
+
+.PHONY: test
+test: test-containers-up test-run
