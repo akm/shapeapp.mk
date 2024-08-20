5d275db33f6957d01ab14b9aec326b3c68ccc3ff
akm <akm2000@gmail.com>
2024-08-20 22:15:15 +0900

    🔨♻️ PATH_TO_PROTO を CONNECT_WEB_PATH_TO_PROTO に改名

--- connect-web/generate.mk
+++ connect-web/generate.mk
@@ -1,7 +1,7 @@
-# Requires:
-# PATH_TO_PROTO?=...
+# Required variables:
+# - CONNECT_WEB_PATH_TO_PROTO
 
 .PHONY: connect-web-generate
 connect-web-generate:
-	$$SHELL -c 'npx buf generate $(PATH_TO_PROTO)/**/*.proto'
+	$$SHELL -c 'npx buf generate $(CONNECT_WEB_PATH_TO_PROTO)/**/*.proto'
 # make can't treat asterisk as a wildcard, so we need to use shell to expand it
