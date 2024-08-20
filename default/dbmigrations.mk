78f8aa111ad73e3abfb3c4515674710512d3b6e2
akm <akm2000@gmail.com>
2024-08-20 21:14:12 +0900

    🔨 default/dbmigrations.mk に主要な定義を移動

--- default/dbmigrations.mk
+++ default/dbmigrations.mk
@@ -1,5 +1,26 @@
-# Requires
-# - include $(PATH_TO_SHAPEAPPMK)/goose/create.mk
+# Required variables:
+# - PATH_TO_SHAPEAPPMK
+# - APP_ENV
+# - APP_STAGE
+# - APP_STAGE_TYPE
+#
+# Optional variables:
+# - GOOSE_TARGET_ENV_PATH
+#
+# included targets:
+# - goose/commands.mk
+
+include $(PATH_TO_SHAPEAPPMK)/golang/base.mk
+
+GOOSE_TARGET_ENV_PATH?=$(PATH_TO_ROOT)/stages/local
+GOOSE_DSN?=$(shell $(MAKE) -C $(GOOSE_TARGET_ENV_PATH) mysql-dsn)
+GOOSE_MAIN_PACKAGE=./cmd/runner
+GOOSE_ENVS=\
+	APP_ENV=$(APP_ENV) \
+	APP_STAGE=$(APP_STAGE) \
+	APP_STAGE_TYPE=$(APP_STAGE_TYPE)
+
+include $(PATH_TO_SHAPEAPPMK)/goose/commands.mk
 
 ifeq (new,$(firstword $(MAKECMDGOALS)))
   NEW_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
