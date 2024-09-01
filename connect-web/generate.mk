b9d0a966ef98b8ac63726a51f0ed1c5e2535a1f6
akm <akm2000@gmail.com>
2024-09-01 22:23:51 +0900

    🔨 connect-web-generate を GitHub では SHELL 変数を使わないように変更

--- connect-web/generate.mk
+++ connect-web/generate.mk
@@ -3,5 +3,9 @@
 
 .PHONY: connect-web-generate
 connect-web-generate:
+ifdef GITHUB_ACTIONS
+	npx buf generate $(CONNECT_WEB_PATH_TO_PROTO)/**/*.proto
+else
 	$$SHELL -c 'npx buf generate $(CONNECT_WEB_PATH_TO_PROTO)/**/*.proto'
+endif
 # make can't treat asterisk as a wildcard, so we need to use shell to expand it
