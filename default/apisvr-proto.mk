62a5cc0294fa13fa5091943efcee4940ac096b5f
akm <akm2000@gmail.com>
2024-09-01 18:58:45 +0900

    🔨 build と generate の依存として PROTOC_GEN_GO_CLI を追加

--- default/apisvr-proto.mk
+++ default/apisvr-proto.mk
@@ -4,13 +4,13 @@ include $(PATH_TO_SHAPEAPPMK)/protobuf/protoc-gen-go.mk
 include $(PATH_TO_SHAPEAPPMK)/connect-go/protoc-gen-connect-go.mk
 
 .PHONY: build
-build: $(PROTOC_GEN_CONNECT_GO_CLI) buf-build
+build: $(PROTOC_GEN_CONNECT_GO_CLI) $(PROTOC_GEN_GO_CLI) buf-build
 
 .PHONY: lint
 lint: buf-lint
 
 .PHONY: generate
-generate: $(PROTOC_GEN_CONNECT_GO_CLI) buf-generate
+generate: $(PROTOC_GEN_CONNECT_GO_CLI) $(PROTOC_GEN_GO_CLI) buf-generate
 
 include $(PATH_TO_SHAPEAPPMK)/grpcurl/base.mk
 
