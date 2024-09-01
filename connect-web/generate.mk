5a1d11bc09abb11ffe58e49158b9c6f8d9e3a097
akm <akm2000@gmail.com>
2024-09-02 00:16:16 +0900

    🔨 connect-web-generate で 対象の .proto を指定せず、ディレクトリだけ指定

--- connect-web/generate.mk
+++ connect-web/generate.mk
@@ -1,8 +1,6 @@
 # Required variables:
 # - CONNECT_WEB_PATH_TO_PROTO
 
-CONNECT_WEB_PROTO_FILES=$(shell find $(CONNECT_WEB_PATH_TO_PROTO) -name '*.proto')
-
 .PHONY: connect-web-generate
 connect-web-generate:
-	npx buf generate $(CONNECT_WEB_PROTO_FILES)
+	npx buf generate $(CONNECT_WEB_PATH_TO_PROTO)
