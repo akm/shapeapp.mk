13226386ded93bff487c833a10b91eed2dbcc0bd
akm <akm2000@gmail.com>
2024-09-07 22:10:08 +0900

    💡 APP_STAGE_TYPE と APP_STAGE の説明と求め方を変更

--- default/app_stage.mk
+++ default/app_stage.mk
@@ -1,36 +1,45 @@
 # ## APP_STAGE_TYPE
 #
+# サーバーを動かす環境を総称して APP_STAGE と呼びます。
+# APP_STAGE には以下の４つの種類が想定され、APP_STAGE_TYPE と呼びます。
+#
 # - production
 # - staging
-# - local
-# - github
-#
-# APP_STAGE_TYPE のインスタンスとして STAGE が複数個存在し、リポジトリにそれらの違いをコミットしなければならない場合もありえます。
-# 例えば staging に対して staging1, staging2 それぞれで設定が異なる場合が考えられます。
-# 逆に local のインスタンスとして各開発者の環境はそれぞれ別物ですが、それらの違いをコミットする必要がなければ local1, local2
-# というような Stage を登録する必要はありませんし、通常はそのように行うべきではありません。
-# ただし APP_STAGE_TYPE として local_windows と  local_mac のような区別を行った方が良い場合もあります。
-#
-# ## APP_STAGE の例
-#
-# - production
-# - staging1
-# - staging2
-# - local
-# - github
+# - localdev
+# - localtest
 # - dev
 
-ifeq ($(GITHUB_ACTIONS),true)
-APP_STAGE_TYPE?=github
-else
 APP_STAGE_TYPE?=dev
-endif
 
-# local, github では STAGE は APP_STAGE_TYPE と同一です。デプロイ対象である production や
-# staging では STAGE は APP_STAGE_TYPE と異なり、staging1 などの具体的なステージ名を指定します。
-ifeq ($(APP_STAGE_TYPE),local)
+# ## APP_STAGE
+#
+# APP_STAGE_TYPE のインスタンスとして APP_STAGE が複数個存在し、
+# リポジトリにそれらの違いをコミットしなければならない場合もありえます。
+# 例えば staging に対して staging1, staging2 それぞれで設定が
+# 異なる場合が考えられます。
+# 逆に localdev のインスタンスとして各開発者の環境はそれぞれ別物ですが、
+# それらの違いをコミットする必要がなければ localdev1, localdev2 という
+# ような Stage を登録する必要はありません。
+# ただし APP_STAGE_TYPE として localdev_windows と  localdev_mac 
+# のような区別を行った方が良い場合も考えられます。
+#
+# ### 例
+#
+# - production1
+# - staging1
+# - staging2
+# - localdev
+# - localtest
+# - dev
+
+# localdev, localtest については APP_STAGE は APP_STAGE_TYPE と同一です。
+# デプロイ対象である production や staging では APP_STAGE は APP_STAGE_TYPE
+# と異なり、staging1 などの具体的なステージ名を指定します。
+ifeq ($(APP_STAGE_TYPE),dev)
 APP_STAGE?=$(APP_STAGE_TYPE)
-else ifeq ($(APP_STAGE_TYPE),github)
+else ifeq ($(APP_STAGE_TYPE),local)
+APP_STAGE?=$(APP_STAGE_TYPE)
+else ifeq ($(APP_STAGE_TYPE),localtest)
 APP_STAGE?=$(APP_STAGE_TYPE)
 else
 APP_STAGE?=$(APP_STAGE_TYPE)1
