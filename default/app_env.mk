28aa831577729ff72d9f05e8cd52a3f8396bc8b6
akm <akm2000@gmail.com>
2024-09-07 21:00:13 +0900

    💡 APP_ENV のコメントを更新

--- default/app_env.mk
+++ default/app_env.mk
@@ -6,8 +6,8 @@
 # ---------|------------------
 # - server | uisvr, apisvr
 # - dev     | uisvr, apisvr, mysql, firebase_auth
-# - e2e_test | uisvr, apisvr, mysql, firebase_auth, clients/uisvr/test/integration
-# - unit_test | uisvr のテスト( clients/uisvr/test/integration 以外), servers 以下のテスト
+# - e2e_test | uisvr, apisvr, mysql, firebase_auth, frontends/uisvr/tests
+# - unit_test | uisvr のテスト( frontends/uisvr/tests 以外), servers 以下のテスト, mysql, firebase_auth
 #
 # この環境変数は、Makefie で処理を開始する際に設定します。
 # つまりmakeのターゲットの実行時にのみ決定するので、静的に指定するものではありません。
