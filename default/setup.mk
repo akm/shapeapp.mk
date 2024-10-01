424716b1f9a1eb377b3614fa394cdb19236514f8
akm <akm2000@gmail.com>
2024-10-01 22:20:05 +0900

    🔨text-tempalte-cli をインストールする前に golang をインストール

--- default/setup.mk
+++ default/setup.mk
@@ -3,8 +3,11 @@
 
 SETUP_GOLANG_VERSION=$(shell at default/templates/.tool-versions | grep golang | cut -d ' ' -f 2)
 
-.PHONY: setup
-setup: $(TEXT_TEMPLATE_CLI)
+.PHONY: setup-golang
+setup-golang:
 	asdf install golang $(SETUP_GOLANG_VERSION)
 	asdf shell golang $(SETUP_GOLANG_VERSION)
+
+.PHONY: setup
+setup: setup-golang $(TEXT_TEMPLATE_CLI)
 	$(CONFIG_ENVS) $(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/default/templates --output-directory $(CURDIR)
