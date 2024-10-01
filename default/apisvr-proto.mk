8f0427fb521bf3991ec4452d8c54d309b12d585d
akm <akm2000@gmail.com>
2024-10-01 23:27:09 +0900

    ♻️ connect-go を components/atoms に移動

--- default/apisvr-proto.mk
+++ default/apisvr-proto.mk
@@ -2,7 +2,7 @@ include $(PATH_TO_SHAPEAPPMK)/git/check.mk
 include $(PATH_TO_SHAPEAPPMK)/components/atoms/buf/buf.mk
 include $(PATH_TO_SHAPEAPPMK)/components/atoms/buf/proto/base.mk
 include $(PATH_TO_SHAPEAPPMK)/protobuf/protoc-gen-go.mk
-include $(PATH_TO_SHAPEAPPMK)/connect-go/protoc-gen-connect-go.mk
+include $(PATH_TO_SHAPEAPPMK)/components/atoms/connect-go/protoc-gen-connect-go.mk
 
 .PHONY: build
 build: $(PROTOC_GEN_CONNECT_GO_CLI) $(PROTOC_GEN_GO_CLI) buf-build
