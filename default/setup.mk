e7fdee8dc4ea173052a432898bc578aac91b9238
akm <akm2000@gmail.com>
2024-10-01 22:17:43 +0900

    🔨setup 時に asdf で golang をインストールし 一時的にそのバージョンを使用

--- default/setup.mk
+++ default/setup.mk
@@ -1,6 +1,10 @@
 # Requires:
 # include $(PATH_TO_SHAPEAPPMK)/text-template-cli/base.mk
 
+SETUP_GOLANG_VERSION=$(shell at default/templates/.tool-versions | grep golang | cut -d ' ' -f 2)
+
 .PHONY: setup
 setup: $(TEXT_TEMPLATE_CLI)
+	asdf install golang $(SETUP_GOLANG_VERSION)
+	asdf shell golang $(SETUP_GOLANG_VERSION)
 	$(CONFIG_ENVS) $(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/default/templates --output-directory $(CURDIR)
