ac50e6fda13647bf44a3d8d7a401d7ca3deb7878
akm <akm2000@gmail.com>
2024-10-02 08:47:36 +0900

    ♻️ components/cells を components/organs に改名

--- components/cells/apisvr/default.mk
+++
@@ -1,6 +0,0 @@
-include $(PATH_TO_SHAPEAPPMK)/components/organelles/golang/base.mk
-include $(PATH_TO_SHAPEAPPMK)/components/cells/apisvr/buf.mk
-include $(PATH_TO_SHAPEAPPMK)/components/cells/apisvr/env.mk
-include $(PATH_TO_SHAPEAPPMK)/components/cells/apisvr/dev.mk
-include $(PATH_TO_SHAPEAPPMK)/components/cells/apisvr/test.mk
-include $(PATH_TO_SHAPEAPPMK)/components/cells/apisvr/docker-image.mk
---
+++ components/organs/apisvr/default.mk
@@ -0,0 +1,6 @@
+include $(PATH_TO_SHAPEAPPMK)/components/organelles/golang/base.mk
+include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/buf.mk
+include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/env.mk
+include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/dev.mk
+include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/test.mk
+include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/docker-image.mk
