fc692b137710323f147fae1cdeee8654b26564df
akm <akm2000@gmail.com>
2024-10-03 07:49:54 +0900

    ♻️ components/atoms/make へのパスを修正

--- components/molecules/make/default.mk
+++ components/molecules/make/default.mk
@@ -1,6 +1,6 @@
 # Required variables:
 # - PATH_TO_SHAPEAPPMK
 
-include $(PATH_TO_SHAPEAPPMK)/make/debug.mk
-include $(PATH_TO_SHAPEAPPMK)/make/or.mk
-include $(PATH_TO_SHAPEAPPMK)/make/shell.mk
+include $(PATH_TO_SHAPEAPPMK)/components/atoms/make/debug.mk
+include $(PATH_TO_SHAPEAPPMK)/components/atoms/make/or.mk
+include $(PATH_TO_SHAPEAPPMK)/components/atoms/make/shell.mk
