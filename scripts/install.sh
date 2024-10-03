f837a6687b71a5e8217837bed1e02b224553beb9
akm <akm2000@gmail.com>
2024-10-03 23:28:18 +0900

    🔨 .shapeapp.mk で components/atoms/git-exec-cli/base.mk を include

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,6 +28,7 @@ PATH_TO_SHAPEAPPMK=\$(PATH_TO_ROOT)/vendor/shapeappmk
 include \$(PATH_TO_SHAPEAPPMK)/components/molecules/make/default.mk
 include \$(PATH_TO_SHAPEAPPMK)/components/atoms/asdf/reshim.mk
 include \$(PATH_TO_SHAPEAPPMK)/components/atoms/golang/tool.mk
+include \$(PATH_TO_SHAPEAPPMK)/components/atoms/git-exec-cli/base.mk
 include \$(PATH_TO_SHAPEAPPMK)/components/atoms/text-template-cli/base.mk
 include \$(PATH_TO_SHAPEAPPMK)/default/app_stage.mk
 include \$(PATH_TO_SHAPEAPPMK)/default/ports.mk
