05237d009e2c850474f0d4b5005eb73b6830eceb
akm <akm2000@gmail.com>
2024-10-03 23:28:47 +0900

    🔨 default/setup.mk で GIT_EXEC_CLI を使うように

--- default/setup.mk
+++ default/setup.mk
@@ -1,4 +1,5 @@
 # Requires:
+# include $(PATH_TO_SHAPEAPPMK)/components/atoms/git-exec-cli/base.mk
 # include $(PATH_TO_SHAPEAPPMK)/components/atoms/text-template-cli/base.mk
 
 SETUP_GOLANG_VERSION=$(shell cat $(PATH_TO_SHAPEAPPMK)/templates/.tool-versions | grep golang | cut -d ' ' -f 2)
@@ -9,5 +10,5 @@ setup-golang:
 	asdf local golang $(SETUP_GOLANG_VERSION)
 
 .PHONY: setup
-setup: setup-golang $(TEXT_TEMPLATE_CLI)
-	$(CONFIG_ENVS) $(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/templates --output-directory $(CURDIR)
+setup: setup-golang $(TEXT_TEMPLATE_CLI) $(GIT_EXEC_CLI)
+	$(GIT_EXEC_CLI) $(CONFIG_ENVS) $(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/templates --output-directory $(CURDIR)
