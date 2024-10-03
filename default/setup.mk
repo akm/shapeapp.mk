fe892338d4a90fa4b76a5ab20db2bbdd9d81fde5
akm <akm2000@gmail.com>
2024-10-03 23:52:21 +0900

    🔨 setup-golang で text-template-cli と git-exec をインストール
    setup の 依存ターゲットに指定すると git-exec の方がインストールされなかったので

--- default/setup.mk
+++ default/setup.mk
@@ -8,7 +8,9 @@ SETUP_GOLANG_VERSION=$(shell cat $(PATH_TO_SHAPEAPPMK)/templates/.tool-versions 
 setup-golang:
 	asdf install golang $(SETUP_GOLANG_VERSION)
 	asdf local golang $(SETUP_GOLANG_VERSION)
+	$(MAKE) $(TEXT_TEMPLATE_CLI) 
+	$(MAKE) $(GIT_EXEC_CLI)
 
 .PHONY: setup
-setup: setup-golang $(TEXT_TEMPLATE_CLI) $(GIT_EXEC_CLI)
+setup: setup-golang
 	$(GIT_EXEC_CLI) $(CONFIG_ENVS) $(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/templates --output-directory $(CURDIR)
