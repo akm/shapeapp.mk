cb6f03835c825d7a314e7a4bceae67ab2b43c39f
akm <akm2000@gmail.com>
2024-10-03 21:47:08 +0900

    🔨 components/atoms/git-exec-cli を追加

---
+++ components/atoms/git-exec-cli/base.mk
@@ -0,0 +1,5 @@
+GIT_EXEC_CLI_VERSION?=v0.1.5
+GIT_EXEC_CLI_MODULE=github.com/akm/git-exec
+GIT_EXEC_CLI?=$(call golang-tool-bin-path,$(GIT_EXEC_CLI_MODULE))
+$(GIT_EXEC_CLI):
+	$(call golang-tool-cli-install-with-prefix,GIT_EXEC)
