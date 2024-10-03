d2e1911c8e328608cdc448c3a1450e74c01a6493
akm <akm2000@gmail.com>
2024-10-04 08:28:00 +0900

    ⬆️ git-exec のバージョンを 0.0.3 に更新

--- components/atoms/git-exec-cli/base.mk
+++ components/atoms/git-exec-cli/base.mk
@@ -1,4 +1,4 @@
-GIT_EXEC_CLI_VERSION?=v0.0.2
+GIT_EXEC_CLI_VERSION?=v0.0.3
 GIT_EXEC_CLI_MODULE=github.com/akm/git-exec
 GIT_EXEC_CLI?=$(call golang-tool-bin-path,$(GIT_EXEC_CLI_MODULE))
 $(GIT_EXEC_CLI):
