70fc4a0fc2a385476c600aa60a262e8bbc83ca02
akm <akm2000@gmail.com>
2024-10-03 23:36:11 +0900

    🔨 Add setup.mk for dbmigrations

---
+++ components/organs/dbmigrations/setup.mk
@@ -0,0 +1,7 @@
+# Requires:
+# include $(PATH_TO_SHAPEAPPMK)/components/atoms/git-exec-cli/base.mk
+
+.PHONY: setup
+setup: $(GIT_EXEC_CLI)
+	$(GIT_EXEC_CLI) go mod init dbmigrations
+	$(GIT_EXEC_CLI) got mod tidy
