f8b85be9b0b5194e27734df845b45fc3f88c2254
akm <akm2000@gmail.com>
2024-10-06 20:19:22 +0900

    🔨 tex-template-cli のオプション名を修正
    make connect-web-install も実行

--- components/organs/frontends/setup.mk
+++ components/organs/frontends/setup.mk
@@ -3,5 +3,6 @@ UISVR=uisvr
 # https://kit.svelte.jp/docs/creating-a-project
 $(UISVR):
 	npm create svelte@latest $(UISVR)
-	cd $(UISVR) && npm install
-	$(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-templates --directory $(UISVR)
+	$(MAKE) -C uisvr npm-install
+	$(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-templates --output-directory $(UISVR)
+	$(MAKE) -C uisvr connect-web-install
