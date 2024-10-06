6ea6744b8b88f5197c8d8bb6db1c887fac4eab16
akm <akm2000@gmail.com>
2024-10-06 20:23:20 +0900

    🔨 npm create svelte@latest  終了後にまず Makefile 等をコピーするように変更

--- components/organs/frontends/setup.mk
+++ components/organs/frontends/setup.mk
@@ -3,6 +3,5 @@ UISVR=uisvr
 # https://kit.svelte.jp/docs/creating-a-project
 $(UISVR):
 	npm create svelte@latest $(UISVR)
-	$(MAKE) -C uisvr npm-install
 	$(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-templates --output-directory $(UISVR)
-	$(MAKE) -C uisvr connect-web-install
+	$(MAKE) -C uisvr npm-install connect-web-install
