4a10ae5b02e70f38741a00c88272d686d3d483bd
akm <akm2000@gmail.com>
2024-10-06 21:32:45 +0900

    🔨make -C frontends uisvr の最後に手動での変更が必要な操作の手順を出力

--- components/organs/frontends/setup.mk
+++ components/organs/frontends/setup.mk
@@ -5,3 +5,4 @@ $(UISVR):
 	npm create svelte@latest $(UISVR)
 	$(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-templates --output-directory $(UISVR)
 	$(MAKE) -C $(UISVR) npm-install connect-web-install connect-node-install
+	@cat $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-instruction.txt
