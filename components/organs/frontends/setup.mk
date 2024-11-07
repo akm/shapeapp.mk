3704c649143dd8463676e76886fb7bb4561a7807
akm <akm2000@gmail.com>
2024-11-08 07:35:54 +0900

    ✨ Use npx sv create instead of npm create svelte
    Because
    
    $ npm create svelte@latest uisvr
    
    > npx
    > create-svelte uisvr
    
    'npm create svelte' has been replaced with 'npx sv create'

--- components/organs/frontends/setup.mk
+++ components/organs/frontends/setup.mk
@@ -2,7 +2,7 @@ UISVR=uisvr
 
 # https://kit.svelte.jp/docs/creating-a-project
 $(UISVR):
-	npm create svelte@latest $(UISVR)
+	npx sv create $(UISVR)
 	$(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-templates --output-directory $(UISVR)
 	$(MAKE) -C $(UISVR) npm-install connect-web-install connect-node-install
 	@cat $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-instruction.txt
