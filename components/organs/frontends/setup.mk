0aeb7443414f91ede7ae825643942286e45daf5c
akm <akm2000@gmail.com>
2024-11-08 08:20:48 +0900

    ✨ Split original installation of svelte-kit and the others

--- components/organs/frontends/setup.mk
+++ components/organs/frontends/setup.mk
@@ -2,7 +2,11 @@ UISVR=uisvr
 
 # https://kit.svelte.jp/docs/creating-a-project
 $(UISVR):
-	npx sv create $(UISVR)
+	@echo "1. Run 'npx sv create $(UISVR)'"
+	@echo "2. Run 'make -C frontends $(UISVR)-setup'"
+
+.PHONY: $(UISVR)-setup
+$(UISVR)-setup: $(UISVR)
 	$(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-templates --output-directory $(UISVR)
 	$(MAKE) -C $(UISVR) npm-install connect-web-install connect-node-install
 	@cat $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-instruction.txt
