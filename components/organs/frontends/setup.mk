f167b6393f1d9ce774a3c1e8d0dcc840adedac91
akm <akm2000@gmail.com>
2024-11-10 21:41:59 +0900

    ✨ Install @sveltejs/adapter-node on make uisvr-setup
    and improve uisvr-instruction.txt

--- components/organs/frontends/setup.mk
+++ components/organs/frontends/setup.mk
@@ -9,7 +9,7 @@ $(UISVR):
 $(UISVR)-setup: $(UISVR)
 	$(TEXT_TEMPLATE_CLI) $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-templates --output-directory $(UISVR)
 	$(MAKE) -C $(UISVR) npm-install connect-web-install connect-node-install
-	cd $(PATH_TO_UISVR) && npm install dotenv
+	cd $(PATH_TO_UISVR) && npm install dotenv && npm install -D @sveltejs/adapter-node
 	@cat $(PATH_TO_SHAPEAPPMK)/components/organs/frontends/uisvr-instruction.txt
 
 # dotenv is required to run on nodejs.
