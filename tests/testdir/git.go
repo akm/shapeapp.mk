db902dab482d2c83135750840d1246d1b47461b8
akm <akm2000@gmail.com>
2024-12-08 21:23:59 +0900

    ♻️ Extract testdir/golang.got

--- tests/testdir/git.go
+++ tests/testdir/git.go
@@ -33,26 +33,3 @@ func GitRoot(t *testing.T) string {
 func FromGitRoot(t *testing.T, relpath string) string {
 	return filepath.Join(GitRoot(t), relpath)
 }
-
-func GoModRoot(t *testing.T) string {
-	dir, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("Failed to get current directory: %v", err)
-	}
-
-	for {
-		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
-			return dir
-		}
-
-		parent := filepath.Dir(dir)
-		if parent == dir {
-			t.Fatalf("go.mod file not found")
-		}
-		dir = parent
-	}
-}
-
-func FromGoModRoot(t *testing.T, relpath string) string {
-	return filepath.Join(GoModRoot(t), relpath)
-}
