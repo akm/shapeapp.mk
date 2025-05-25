7a553db2b7dcf76cda11a1f7110f95a56714e7af
akm <akm2000@gmail.com>
2025-05-25 17:29:44 +0900

    🔨 apisvr で make proto できるように追加

--- components/organs/apisvr/default.mk
+++ components/organs/apisvr/default.mk
@@ -4,3 +4,4 @@ include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/env.mk
 include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/dev.mk
 include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/test.mk
 include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/docker-image.mk
+include $(PATH_TO_SHAPEAPPMK)/components/organs/apisvr/proto.mk
