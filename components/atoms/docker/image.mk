4ce982ab2f65b399fff103fff868c18488ad9443
akm <akm2000@gmail.com>
2025-06-15 15:37:08 +0900

    ✨ ターゲットdocker-image-buildx を追加
    docker-image-build とは異なり DOCKER_IMAGE_PLATFORM でプラットフォームを指定可能

--- components/atoms/docker/image.mk
+++ components/atoms/docker/image.mk
@@ -5,9 +5,12 @@
 # Options:
 # - DOCKER_IMAGE_BUILD_DEPS
 # - DOCKER_IMAGE_BUILD_OPTS: Default: -f Dockerfile
+# - DOCKER_IMAGE_PLATFORM: Default: linux/amd64
+
 
 DOCKER_IMAGE_BUILD_DEPS?=
 DOCKER_IMAGE_BUILD_OPTS?=-f Dockerfile
+DOCKER_IMAGE_PLATFORM?=linux/amd64
 
 .PHONY: docker-image-name
 docker-image-name:
@@ -16,6 +19,14 @@ docker-image-name:
 .PHONY: docker-image-build
 docker-image-build: $(DOCKER_IMAGE_BUILD_DEPS)
 	docker build \
+		-t $(DOCKER_IMAGE_NAME) \
+		$(DOCKER_IMAGE_BUILD_OPTS) \
+		.
+
+.PHONY: docker-image-buildx
+docker-image-buildx: $(DOCKER_IMAGE_BUILD_DEPS)
+	docker buildx build \
+		--platform $(DOCKER_IMAGE_PLATFORM) \
 		-t $(DOCKER_IMAGE_NAME) \
 		$(DOCKER_IMAGE_BUILD_OPTS) \
 		.
