9495b51b801dd5ad1f02efa47e15b9db664f3615
akm <akm2000@gmail.com>
2026-01-01 18:09:35 +0900

    🔨 connectrpc-install-tools から grpcurl を対象外に
    grpcurl が必要な場合は別途以下を実行
    make grpcurl-cli-install

--- components/organelles/connectrpc/base.mk
+++ components/organelles/connectrpc/base.mk
@@ -1,20 +1,17 @@
 include $(PATH_TO_SHAPEAPPMK)/components/atoms/buf/buf.mk
 include $(PATH_TO_SHAPEAPPMK)/components/atoms/buf/proto/base.mk
 include $(PATH_TO_SHAPEAPPMK)/components/atoms/connect-go/protoc-gen-connect-go.mk
-include $(PATH_TO_SHAPEAPPMK)/components/atoms/grpcurl/base.mk
 include $(PATH_TO_SHAPEAPPMK)/components/atoms/protobuf/protoc-gen-go.mk
 
 .PHONY: connectrpc-install-tools
 connectrpc-install-tools:
 	$(MAKE) $(BUF_CLI)
-	$(MAKE) $(GRPCURL_CLI)
 	$(MAKE) $(PROTOC_GEN_GO_CLI)
 	$(MAKE) $(PROTOC_GEN_CONNECT_GO_CLI)
 
 .PHONY: connectrpc-update-tools
 connectrpc-update-tools: \
 	buf-cli-install \
-	grpcurl-cli-install \
 	protoc-gen-go-cli-install \
 	protoc-gen-connect-go-cli-install
 
