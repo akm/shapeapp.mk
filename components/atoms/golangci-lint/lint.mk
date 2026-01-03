e30afe486ce55ee44b088cf171d20a7e5a98208e
akm <akm2000@gmail.com>
2026-01-03 21:17:28 +0900

    🔨 golangci-lint の v2 に対応
    GOLANGCLI_LINT_CLI_VERSION_BASE に v2 を指定することで利用可能に

--- components/atoms/golangci-lint/lint.mk
+++ components/atoms/golangci-lint/lint.mk
@@ -1,5 +1,8 @@
+GOLANGCLI_LINT_CLI_VERSION_BASE?=v1
 GOLANGCLI_LINT_CLI_VERSION?=latest
-GOLANGCLI_LINT_CLI_MODULE=github.com/golangci/golangci-lint/cmd/golangci-lint
+GOLANGCLI_LINT_CLI_MODULE_v1=github.com/golangci/golangci-lint/cmd/golangci-lint
+GOLANGCLI_LINT_CLI_MODULE_v2=github.com/golangci/golangci-lint/v2/cmd/golangci-lint
+GOLANGCLI_LINT_CLI_MODULE=$(GOLANGCLI_LINT_CLI_MODULE_$(GOLANGCLI_LINT_CLI_VERSION_BASE))
 GOLANGCLI_LINT_CLI=$(call golang-tool-bin-path,$(GOLANGCLI_LINT_CLI_MODULE))
 $(GOLANGCLI_LINT_CLI):
 	$(call golang-tool-cli-install-with-prefix,GOLANGCLI_LINT)
